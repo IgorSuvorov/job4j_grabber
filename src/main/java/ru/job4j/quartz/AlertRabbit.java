7435d0a438607927183fbf9a6d30f633f58f2e1e
isuvorov1 <isuvorov1@outlook.com>
2022-01-23 04:36:09 -0500

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,57 @@
+package ru.job4j.quartz;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+
+    private static int rabbitInterval = 0;
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader("rabbit.properties"))) {
+            read.lines().filter(line -> line.length() > 0)
+                    .forEach(line -> {
+                        if (line.startsWith("=") || line.endsWith("=")) {
+                            throw new IllegalArgumentException("no key or value");
+                        }
+                        String[] keyValue = line.split("=");
+                        rabbitInterval = Integer.parseInt(keyValue[1]);
+                    });
+        } catch (IOException e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(rabbitInterval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
