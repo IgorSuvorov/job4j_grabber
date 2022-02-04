166dadc9766d7bda947f00cbc5aeceffd2f91158
isuvorov1 <isuvorov1@outlook.com>
2022-02-04 17:44:49 -0500

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,33 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            entry("янв", "01"),
+            entry("фев", "02"),
+            entry("мар", "03"),
+            entry("апр", "04"),
+            entry("май", "05"),
+            entry("июн", "06"),
+            entry("июл", "07"),
+            entry("авг", "08"),
+            entry("сен", "09"),
+            entry("окт", "10"),
+            entry("ноя", "11"),
+            entry("дек", "12"),
+            entry("сегодня", LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("d-MM-yy"))),
+            entry("вчера", LocalDateTime.now()
+                    .minusDays(1).format(DateTimeFormatter.ofPattern("d-MM-yy"))));
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        return null;
+    }
+}
