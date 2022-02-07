d91749098a0bb0976d493cde191dedd7359c60be
isuvorov1 <isuvorov1@outlook.com>
2022-02-07 18:45:35 -0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,8 +4,28 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
 
 public class SqlRuParse {
+    public Post postData(String url) {
+        SqlRuDateTimeParser sq = new SqlRuDateTimeParser();
+        Post post = new Post();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".msgTable");
+            String description = row.first().select(".msgBody").get(1).text();
+            String title = row.first().select(".messageHeader").text();
+            String created = row.last().select(".msgFooter").text();
+            return new Post(title, url, description, sq.parse(created));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
     public static void main(String[] args) throws Exception {
         for (int page = 1; page <= 5; page++) {
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
