dd31632f3ed6bcfff086138e640dd138abebc084
isuvorov1 <isuvorov1@outlook.com>
2022-02-09 17:58:04 -0500

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,26 +4,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
-    public Post postData(String url) {
-        SqlRuDateTimeParser sq = new SqlRuDateTimeParser();
-        Post post = new Post();
-        try {
-            Document doc = Jsoup.connect(url).get();
-            Elements row = doc.select(".msgTable");
-            String description = row.first().select(".msgBody").get(1).text();
-            String title = row.first().select(".messageHeader").text();
-            String created = row.last().select(".msgFooter").text();
-            return new Post(title, url, description, sq.parse(created));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return post;
+public class SqlRuParse implements Parse {
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
     public static void main(String[] args) throws Exception {
@@ -38,5 +32,37 @@ public class SqlRuParse {
                 System.out.println(date);
             }
         }
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> posts = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                posts.add(detail(td.child(0).attr("href")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post detail(String link) {
+        SqlRuDateTimeParser sq = new SqlRuDateTimeParser();
+        Post post = new Post();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".msgTable");
+            String description = row.first().select(".msgBody").get(1).text();
+            String title = row.first().select(".messageHeader").text();
+            String created = row.last().select(".msgFooter").text();
+            return new Post(title, link, description, sq.parse(created));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
     }
 }
